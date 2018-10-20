6355249b6a21cc14c5b87fb3341b6d58f7b16b26
AmadeusChan <greenclouds@foxmail.com>
2018-10-21 01:03:43 +0800

    already finished liveness

--- src/submit/MySolver.java
+++ src/submit/MySolver.java
@@ -3,6 +3,7 @@ package submit;
 // some useful things to import. add any additional imports you need.
 import joeq.Compiler.Quad.*;
 import flow.Flow;
+import java.util.*;
 
 /**
  * Skeleton class for implementing the Flow.Solver interface.
@@ -10,6 +11,19 @@ import flow.Flow;
 public class MySolver implements Flow.Solver {
 
     protected Flow.Analysis analysis;
+
+    //public class BackwardBasicBlockVisitor extends QuadVisitor.EmptyVisitor implements BasicBlockVisitor {
+    //        private isChanged = false;
+    //        void visitBasicBlock(BasicBlock bb) {
+    //    	    if (bb.isEntry() || bb.isExit()) return ;
+    //    	    // running from last Quad to the first one 
+    //    	    ListIterator<Quad> quadIter = bb.iterator();
+    //    	    ArrayList<Quad> quadList = new ArrayList<Quad>();
+    //    	    while (quadIter.hasNext()) quadIter.next();
+
+    //    	    // update the IN using OUT
+    //        }
+    //}
 
     /**
      * Sets the analysis.  When visitCFG is called, it will
@@ -36,6 +50,58 @@ public class MySolver implements Flow.Solver {
         /***********************
          * Your code goes here *
          ***********************/
+
+	if (analysis.isForward()) {
+		//boolean isChanged = true;
+		//while (isChanged) {
+		//	isChanged = false;
+		//	QuadIterator qit = new QuadIterator(cfg);
+		//	while (qit.hasNext()) {
+		//	}
+		//}
+	} else {
+		boolean isChanged = true;
+		while (isChanged) {
+			isChanged = false;
+			QuadIterator qit = new QuadIterator(cfg);
+			while (qit.hasNext()) {
+				Quad q = qit.next();
+				Flow.DataflowObject newOut = analysis.newTempVar();
+				newOut.setToTop();
+				Iterator<Quad> succIter = qit.successors();
+				while (succIter.hasNext()) {
+					Quad succ = succIter.next();
+					if (succ == null) { // which indicates that this quad is connected to EXIT
+						newOut.meetWith(analysis.getExit());
+					} else {
+						newOut.meetWith(analysis.getIn(succ));
+					}
+				}
+				// so far we have already got the OUT orginal from the IN[all the successors]
+				Flow.DataflowObject oldIn = analysis.getIn(q);
+				analysis.setOut(q, newOut);
+				analysis.processQuad(q);
+				if (!oldIn.equals(analysis.getIn(q))) {
+					isChanged = true;
+				}
+			}
+		}
+		Flow.DataflowObject entryValue = analysis.newTempVar();
+		entryValue.setToTop();
+		QuadIterator qit = new QuadIterator(cfg);
+		while (qit.hasNext()) {
+			Quad q = qit.next();
+			Iterator<Quad> preIter = qit.predecessors();
+			while (preIter.hasNext()) {
+				Quad pre = preIter.next();
+				if (pre == null) {
+					entryValue.meetWith(analysis.getIn(q));
+					break;
+				}
+			}
+		}
+		analysis.setEntry(entryValue);
+	} 
 
         // this needs to come last.
         analysis.postprocess(cfg);
