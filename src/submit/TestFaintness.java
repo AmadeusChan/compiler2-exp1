86d8f0d596842d85fa554bda7863942d9f30217d
Amadeus Chan <greenclouds@foxmail.com>
2018-10-21 16:28:09 +0800

    added testcases

--- src/submit/TestFaintness.java
+++ src/submit/TestFaintness.java
@@ -22,4 +22,84 @@ class TestFaintness {
       }
       ...
     */
+
+    /**
+     * These test cases are designed according to the following principles:
+     * (1) there is no MOVE/BINARY quad
+     * 		(a) all the variables are faint
+     * 		(b) some of the variables are faint
+     * 		(c) none of the variables is faint
+     * (2) there is some MOVE/BINARY quad
+     * 		(a) all the defined variable in this quad are faint
+     * 		(b) none of the defined variable in this quad is faint
+     * (3) some additional case concerning branch
+     */
+
+    /**
+     * no MOVE/BINARY: all the variable are faint
+     */
+    void test2() {
+	    int a = 1;
+	    int b = 2;
+	    int c = 3;
+	    int d = 4;
+	    int e = 5;
+    }
+
+    /**
+     * no MOVE/BINARY: some of the variable are faint, in this case, all variables except a are faint since only the value of a is finally used
+     */
+    void test3() {
+	    int a = 1;
+	    int b = 2;
+	    int c = 3;
+	    return a;
+    }
+
+    /**
+     * no MOVE/BINARY: none of the variables is faint since they all contribute to the final output value
+     */
+    void test4() {
+	    int a = 1;
+	    int b = 2;
+	    int c = 3;
+	    return a + b + c;
+    }
+
+    /**
+     * with MOVE/BINARY: all the defined variables in the MOVE/BINARY quad are fiant
+     * fant-variables: a, b, c 
+     * reason: these variables are never truly used, a and b are only used to calculate another dead variable c
+     */
+    void test5() {
+	    int a = 1;
+	    int b = 2;
+	    int c = a + b;
+    }
+
+    /**
+     * with MOVE/BINARY: none of the defined variable in the MOVE/BINARY quad are faint    
+     * faint-variables: none
+     * reason: since c is not faint, and as a result, these variables used to calculate c are not faint
+     */
+    void test6() {
+	    int a = 1;
+	    int b = 2;
+	    int c = a + b;
+	    return c;
+    }
+
+    /** 
+     * some additional case concerning branch
+     * faint-variables: none
+     * reason: since a may be used in one of the if case
+     */
+    void test7() {
+	    int a = 1 + 1000;
+	    if (a > 10) {
+		    return a;
+	    } else {
+		    return 0;
+	    }
+    }
 }
